0cc42a6cc138e34ffed652674b3acbed147fa493
fatmkii <47155837@qq.com>
2023-05-13 13:54:21 +0800

    demo v1.1

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ css = '''
     max-width: 600px;
 }
 '''
-
+examples = ['./examples/'+file for file in os.listdir('./examples')] # 样本图片集
 YoloV8  = YOLO('./models/yolov8_otters_epoch400_230503.pt')  # 从yolov8迁移训练的
 
 def YoloV8_handle(image):
@@ -23,7 +23,7 @@ def YoloV8_handle(image):
     
     result_plotted = results[0].plot() #numpy的array类型，并且颜色顺序是BGR
     result_cls_list = results[0].boxes.cls.tolist()
-    text = ['# 找到了海獭！','# 找到了水獭！','# 没有找到海獭水獭……']
+    text = ['# 找到了水獭！','# 找到了海獭！','# 没有找到海獭水獭……']
     if(len(result_cls_list)>0):
         result_cls = text[int(result_cls_list[0])]
     else:
@@ -38,22 +38,16 @@ def clear(*args):
 with gr.Blocks(title='是海獭不是水獭',css=css) as demo:
     gr.Markdown("# 海獭和水獭是不同的，和水獭")
     with gr.Tab("YoloV8"):
-        image_1 = gr.Image(type='numpy').style(height=600)
+        image_1 = gr.Image(type='numpy',brush_radius=0).style(height=500)
         text_output_1 = gr.Markdown()
         output_1 = [text_output_1,image_1]
         with gr.Row():
             button_clear = gr.Button("清空",variant="secondary")
             button_1 = gr.Button("提交",variant="primary")
-
-
         gr.Examples(
-        # examples=[os.path.join(os.path.dirname(__file__), "examples/sea_otter.jpg"),
-        #           os.path.join(os.path.dirname(__file__), "examples/river_otter.jpg")],
-        examples=["./examples/sea_otter.jpg",
-                  "./examples/river_otter.jpg"],
-        inputs=image_1,
-        )
-
+            examples=examples,
+            inputs=[image_1]
+            )
 
     button_1.click(YoloV8_handle, inputs=image_1, outputs=output_1)
     button_clear.click(clear, inputs=[image_1,text_output_1], outputs=[image_1,text_output_1])
